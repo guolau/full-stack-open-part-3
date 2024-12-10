f306c0725b7ef8e271e1a36f7e5fba5b5f5d1c90
Laura Guo <lauraguo@proton.me>
2024-12-10 17:31:48 -0500

    Complete exercise 3.4 (add DELETE /api/persons/:id endpoint for phonebook)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const persons = [
+let persons = [
   {
     id: "1",
     name: "Arto Hellas",
@@ -43,6 +43,12 @@ app.get("/api/persons/:id", (request, response) => {
   } else {
     response.status(404).end();
   }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  persons = persons.filter((person) => person.id !== request.params.id);
+
+  response.status(204).end();
 });
 
 const PORT = 3001;
