2d3c04cc6ac7a65b458b6a3fe0468b216c1b3e9b
Laura Guo <lauraguo@proton.me>
2024-12-10 19:11:56 -0500

    Complete exercise 3.8 (add morgan logging for post body in phonebook)

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
+morgan.token("post-body", function (req, res) {
+  return req.method === "POST" ? JSON.stringify(req.body) : "";
+});
+
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :post-body"
+  )
+);
 
 let persons = [
   {
