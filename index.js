1bb270185f87336f7cad483ffd6f11e2c24fdd55
Laura Guo <lauraguo@proton.me>
2024-12-12 21:08:44 -0500

    Complete exercise 3.16 (move error handling to middleware)

--- index.js
+++ index.js
@@ -18,22 +18,26 @@ app.use(
   )
 );
 
-app.get("/info", (request, response) => {
-  Person.find({}).then((persons) => {
-    const phonebookInfo = `Phonebook has info for ${persons.length} people`;
-    const time = new Date().toString();
+app.get("/info", (request, response, next) => {
+  Person.find({})
+    .then((persons) => {
+      const phonebookInfo = `Phonebook has info for ${persons.length} people`;
+      const time = new Date().toString();
 
-    response.send(`<p>${phonebookInfo}</p><p>${time}</p>`);
-  });
+      response.send(`<p>${phonebookInfo}</p><p>${time}</p>`);
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/api/persons", (request, response) => {
-  Person.find({}).then((persons) => {
-    response.json(persons);
-  });
+app.get("/api/persons", (request, response, next) => {
+  Person.find({})
+    .then((persons) => {
+      response.json(persons);
+    })
+    .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
   if (!body.name) {
@@ -63,28 +67,47 @@ app.post("/api/persons", (request, response) => {
     number: body.number,
   });
 
-  person.save().then((result) => {
-    response.json(person);
-  });
-});
-
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    console.log(person);
-    if (person) {
+  person
+    .save()
+    .then((result) => {
       response.json(person);
-    } else {
-      response.status(404).end();
-    }
-  });
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  console.log(request.params);
-  Person.findByIdAndDelete(request.params.id).then((result) => {
-    response.status(204).end();
-  });
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      console.log(person);
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
+
+app.delete("/api/persons/:id", (request, response, next) => {
+  console.log(request.params);
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "Invalid id format" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
